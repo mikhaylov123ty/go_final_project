d473e3a5626bae47e5ee61d70cfdfd8ea02968b6
Alexandr Mikhailov <mikhaylov123ty@gmail.com>
2024-05-01 13:07:26 +0300

    add advanced month handler

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"finalProject/internal/dataBase"
-	"finalProject/internal/tasks"
 	"finalProject/internal/webServer"
-	"fmt"
 	"log"
 	"os"
-	"time"
 )
 
 func main() {
@@ -17,15 +14,6 @@ func main() {
 	}
 
 	defer dbConnect.DB.Close()
-
-	nowTime, err := time.Parse("20060102", "20240126")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	fmt.Println(tasks.NextDate(nowTime, "20240113", "d 7"))
-	fmt.Println(tasks.NextDate(nowTime, "16890220", "y"))
-	fmt.Println(tasks.NextDate(nowTime, "20240126", "k 34"))
-	fmt.Println(tasks.NextDate(nowTime, "20240126", "d 2"))
 
 	server := webServer.Init(os.Getenv("TODO_PORT"))
 	err = server.Start("web")
--- internal/tasks/repeaters.go
+++ internal/tasks/repeaters.go
@@ -2,6 +2,8 @@ package tasks
 
 import (
 	"errors"
+	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -9,8 +11,8 @@ import (
 
 type repeater struct {
 	modifier string
-	value    int
-	currDate time.Time
+	value    [2][]int
+	date     time.Time
 	now      time.Time
 }
 
@@ -20,45 +22,66 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 		return "", err
 	}
 
+	fmt.Println(r)
+
 	switch r.modifier {
 	case "d":
-		r.moveDay()
+		r.moveDays()
 	case "y":
-		r.moveYear()
+		r.moveYears()
+	case "w":
+		r.moveWeeks()
+	case "m":
+		r.moveMonths()
 	default:
 		return "", errors.New("invalid repeat symbol")
 	}
 
-	//now = const 26012024
-	//date = current recursive date
-	//repeat = days or months to add to date until it will be greater now
-	//TODO date + repeate string until it will greater then now
-
-	return r.currDate.String(), nil
+	return r.date.String(), nil
 }
 
 func parseRepeater(now time.Time, date string, repeat string) (*repeater, error) {
-	var repeatVal int
+	var repeatVal [2][]int
 	var err error
 
 	repeatStr := strings.Split(repeat, " ")
 	if len(repeatStr) > 1 {
-		repeatVal, err = strconv.Atoi(repeatStr[1])
-		if err != nil {
-			return nil, err
+		for i := 1; i < len(repeatStr); i++ {
+			repeatStrVal := strings.Split(repeatStr[i], ",")
+			for _, v := range repeatStrVal {
+				val, err := strconv.Atoi(v)
+				if err != nil {
+					return nil, err
+				}
+				repeatVal[i-1] = append(repeatVal[i-1], val)
+			}
 		}
 	} else if repeatStr[0] == "y" {
-		repeatVal = 1
+		repeatVal[0] = append(repeatVal[0], 1)
 	} else {
 		return nil, errors.New("invalid repeat value")
 	}
 
-	if repeatVal > 400 {
+	if repeatVal[0][0] > 400 {
 		return nil, errors.New("invalid repeat value")
 	}
 
+	if repeatVal[0][0] <= -3 {
+		return nil, errors.New("invalid repeat value")
+	}
+
+	if len(repeatVal[1]) == 0 {
+		for i := 1; i <= 12; i++ {
+			repeatVal[1] = append(repeatVal[1], i)
+		}
+	}
+
+	for _, v := range repeatVal {
+		sort.Ints(v)
+	}
+
 	r := &repeater{modifier: repeatStr[0], value: repeatVal, now: now}
-	r.currDate, err = time.Parse("20060102", date)
+	r.date, err = time.Parse("20060102", date)
 	if err != nil {
 		return nil, err
 	}
@@ -66,18 +89,56 @@ func parseRepeater(now time.Time, date string, repeat string) (*repeater, error)
 	return r, nil
 }
 
-func (r *repeater) moveDay() {
-	for r.currDate.Before(r.now) {
-		r.currDate = r.currDate.AddDate(0, 0, r.value)
+func (r *repeater) moveDays() {
+	for r.date.Before(r.now) {
+		r.date = r.date.AddDate(0, 0, r.value[0][0])
 	}
 }
 
-func (r *repeater) moveYear() {
-	for r.currDate.Before(r.now) {
-		r.currDate = r.currDate.AddDate(r.value, 0, 0)
+func (r *repeater) moveYears() {
+	for r.date.Before(r.now) {
+		r.date = r.date.AddDate(r.value[0][0], 0, 0)
 	}
 }
 
-func (r *repeater) moveMonth() {
+func (r *repeater) moveWeeks() {
+	var weekDays string
+	for _, weekDay := range r.value[0] {
+		weekDays += time.Weekday(weekDay).String()
+	}
 
+	for r.date.Before(r.now) || !(strings.Contains(weekDays, r.date.Weekday().String())) {
+		r.date = r.date.AddDate(0, 0, 1)
+	}
+}
+
+func (r *repeater) moveMonths() {
+	dates := make([]time.Time, 0)
+
+	if r.date.Before(r.now) {
+		r.date = r.now
+	}
+
+	for _, month := range r.value[1] {
+		for _, day := range r.value[0] {
+			m := month
+			if day < 0 {
+				m++
+			}
+			parseTime := time.Date(r.now.Year(), time.Month(m), day, 0, 0, 0, 0, time.UTC)
+			dates = append(dates, parseTime)
+		}
+	}
+
+	for len(dates) > 0 {
+		if r.date.Before(dates[0]) {
+			r.date = dates[0]
+			fmt.Println("ANSWER:", r.date)
+			return
+		} else {
+			newDate := dates[0].AddDate(1, 0, 0)
+			dates = dates[1:]
+			dates = append(dates, newDate)
+		}
+	}
 }
